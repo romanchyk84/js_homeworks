78eafd135f72ddb150f23185a610bc40306d8c64
RM <romanchik84@gmail.com>
2025-12-01 09:16:46 +0100

    commit 7

--- domashka_2.js
+++ domashka_2.js
@@ -1,6 +1,6 @@
 function pow(x, y) {
 
-console.log(`Chislo: ${x} v stepeni: ${y}`);
+console.log(x,'v stepeni:', y);
 
 let result = 1;
 for (let i = 0; i < y; i++)
@@ -8,13 +8,19 @@ for (let i = 0; i < y; i++)
 return result
 }
 
+console.log(pow(7, 0));
+razdel();
+
 console.log(pow(7, 2));
 razdel();
+
 console.log(pow(7, 3));
 razdel();
+
 console.log(pow(2, 10));
 razdel();
 
 function razdel() {
     console.log('========================')
-}
+};
+
