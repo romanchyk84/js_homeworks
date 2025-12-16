b519ee968db52401dba324b07f958316d91a164a
RM <romanchik84@gmail.com>
2025-12-16 15:46:34 +0100

    2

---
+++ domashka_4.js
@@ -0,0 +1,11 @@
+// Here will be a fifth homework
+
+var services = {
+    "стрижка": "60 грн",
+    "гоління": "80 грн",
+    "Миття голови": "100 грн"
+    };
+
+console.log(services);
+services['Розбити скло'] = "200 грн";
+console.log(services);
