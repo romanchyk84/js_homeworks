26d96285c5a06f7465ea3d133a24a0b5667ba002
RM <romanchik84@gmail.com>
2025-12-17 16:39:05 +0100

    1

---
+++ Paranoya.js
@@ -0,0 +1,22 @@
+// U nas paranoya. Doveryaem adresam gmail i yahoo
+
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        <email:"dmitro.porohov@yahoo.com>"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+let x = arr[0];
+console.log(x);
